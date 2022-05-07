805cb20c712ce189c9263f64f1a61108ea24b742
Andres Manzano <andres.manzano@exclusivecontent.me>
2022-05-07 08:43:53 -0500

    ..

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8081;
 
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 app.get("/", (req,res,next)=>{
     res.json({response:true});
 })
 
 app.listen(PORT, function() {
-    debug(`Listening http://localhost:${PORT}`);
+    console.log(`Listening http://localhost:${PORT}`);
   });
   
